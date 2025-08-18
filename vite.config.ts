15d96a4bf68e57d6620ca9817f342cc8454b771a
wagnerbaianoo <141805513+wagnerbaianoo@users.noreply.github.com>
2025-08-17 23:16:00 -0300

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
-import { fileURLToPath } from 'url'
-
-const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
-  server: {
-    port: 3000,
-    host: true,
-    historyApiFallback: true,
-  },
-  build: {
-    outDir: 'dist',
-  },
+  css: {
+    postcss: './postcss.config.js'
+  }
 })
