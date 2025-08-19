1984e1056014f6aed1b075e50273aba9559694a5
wagnerbaianoo <141805513+wagnerbaianoo@users.noreply.github.com>
2025-08-19 01:12:38 -0300

    Corrigir problema de preview

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,18 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  experimental: {
+    esmExternals: false,
+  },
+  webpack: (config) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      fs: false,
+      net: false,
+      tls: false,
+    };
+    return config;
+  },
 }
 
 export default nextConfig
