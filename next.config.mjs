de1842e53b9e5fe521b6dcdc680f14741793a02f
wagnerbaianoo <141805513+wagnerbaianoo@users.noreply.github.com>
2025-08-19 01:15:03 -0300

    Updated next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -9,9 +9,6 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  experimental: {
-    esmExternals: false,
-  },
   webpack: (config) => {
     config.resolve.fallback = {
       ...config.resolve.fallback,
