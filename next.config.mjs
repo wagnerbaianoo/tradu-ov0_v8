ea04d89208778e0cd9be7aa33efd2999fd7989b6
wagnerbaianoo <141805513+wagnerbaianoo@users.noreply.github.com>
2025-08-19 02:59:31 -0300

    Updated next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    missingSuspenseWithCSRBailout: false,
-  
-  },
   eslint: {
     ignoreDuringBuilds: true,
   },
